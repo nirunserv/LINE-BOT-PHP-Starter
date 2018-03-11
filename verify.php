b881acab7723a0d8c11a9cfde62e18d183389239
nirunserv <37208217+nirunserv@users.noreply.github.com>
2018-03-11 19:18:52 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'EWwo8d7s4YvBzxU+xPGrYn/BJd4ieRlxNam+Nx84EhNMNrhM0j50Z4sLM1X9CKVPOCmFw0q8dR0SLZ275uNoDrbynSISs787YTBTQCTcvqQB7bCC3kYxsKKW1yFPUEv77yU+3eVTCzPYvv8MlfuBqwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xxxxx';
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
